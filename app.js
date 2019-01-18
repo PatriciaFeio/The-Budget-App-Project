0d4120b1250d505e239db77755878cde384472c1
Patricia Feio <patriciafeio@gmail.com>
2019-01-18 12:44:04 +0000

    Added event listener to button class add__btn

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
-// Data module - budgetController
+// Data module - BUDGET CONTROLLER
 
-var budgetController = (function() {
-  var x = 23;
+var budgetController = (function() {})();
 
-  var add = function(a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
-})();
-
-// UI module
+// UI module - UI CONTROLLER
 
 var UIController = (function() {})();
 
-// Controller module - connects the previous modules
+// Controller module - GLOBAL APP CONTROLLER
+// Connects the previous modules
 
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
-  };
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    // 1. Get the filed input data
+    // 2. Add item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  });
 })(budgetController, UIController);
