9f4a3d70c0f3e3c86b1ee7592f7815cb466cd358
Patricia Feio <patriciafeio@gmail.com>
2019-01-18 13:13:19 +0000

    Added event Listener when pressing ENTER key

--- app.js
+++ app.js
@@ -10,11 +10,20 @@ var UIController = (function() {})();
 // Connects the previous modules
 
 var controller = (function(budgetCtrl, UICtrl) {
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    // 1. Get the filed input data
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
     // 2. Add item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+    // for more older browsers use which instead of keyCode
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
   });
 })(budgetController, UIController);
