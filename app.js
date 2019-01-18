8a1f159708abc2e6442f466badc41d64de929eb3
Patricia Feio <patriciafeio@gmail.com>
2019-01-18 15:44:38 +0000

    Added setupEventListeners function in controller module

--- app.js
+++ app.js
@@ -31,7 +31,20 @@ var UIController = (function() {
 // Connects the previous modules
 
 var controller = (function(budgetCtrl, UICtrl) {
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document
+      .querySelector(DOM.inputButton)
+      .addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+      // for more older browsers use which instead of keyCode
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
   var ctrlAddItem = function() {
     // 1. Get the field input data
@@ -44,15 +57,4 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   };
-
-  document
-    .querySelector(DOM.inputButton)
-    .addEventListener("click", ctrlAddItem);
-
-  document.addEventListener("keypress", function(event) {
-    // for more older browsers use which instead of keyCode
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  });
 })(budgetController, UIController);
