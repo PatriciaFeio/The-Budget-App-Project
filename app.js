317484f384c206726a0168d011f1ada1981b52f9
Patricia Feio <patriciafeio@gmail.com>
2019-01-21 18:04:54 +0000

    Updating the budget: UI controller. New public method in the budget module, displayBudget(). More strings in the DOMstrings.Added UICtrl.displayBudget() to init function

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ var budgetController = (function () {
         totalInc: data.totals.inc,
         totalExp: data.totals.exp,
         percentage: data.percentage
-      }
+      };
     },
 
     testing: function () {
@@ -114,7 +114,11 @@ var UIController = (function () {
     inputValue: '.add__value',
     inputButton: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
 
   };
 
@@ -166,6 +170,20 @@ var UIController = (function () {
 
     },
 
+    displayBudget: function (obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+      }
+
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     }
@@ -199,7 +217,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     // 2. Return the budget
     var budget = budgetCtrl.getBudget();
     // 3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   }
 
   var ctrlAddItem = function () {
@@ -226,6 +244,12 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log("Application has started.");
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
